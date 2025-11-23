0ca11e84c52227611722c9fa1ddb7f736aa54135
Gia Bao Nguyen <thuvien040281@gmail.com>
2025-11-23 12:27:52 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // deploy-commands.js
 // This script registers the Slash Commands with the Discord API. 
 // It should be run manually one time.
@@ -14,6 +13,8 @@ const commands = [
     {
         name: 'setup-auth',
         description: 'Sets up the Synapse Pass Auth Gate for this server.',
+        // TRƯỜNG MỚI: Chỉ cho phép Quản trị viên (Permission Bit 8) sử dụng lệnh
+        default_member_permissions: "8",
         options: [
             {
                 name: 'role',
@@ -55,38 +56,5 @@ const apiEndpoint = `https://discord.com/api/v10/applications/${CLIENT_ID}/comma
         console.log('Successfully registered application (/) commands.');
     } catch (error) {
         console.error('Error deploying commands:', error);
-=======
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
-
-const commands = [];
-const commandFolders = ['free', 'premium', 'admin'];
-
-for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(path.join(__dirname, 'commands', folder)).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(path.join(__dirname, 'commands', folder, file));
-        commands.push(command.data.toJSON());
-    }
-}
-
-const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
-
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID), // You need to add CLIENT_ID to your .env
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
->>>>>>> d7705f512eae99dcf31b28c870815fc7efe23666
     }
 })();
